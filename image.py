f2bed1e1e3c83c5bbcc6849de4f31adb3e9e3da0
vedantpatel1997 <vedantp9@gmail.com>
2025-01-29 22:33:52 -0700

    optimize for memory

--- image.py
+++ image.py
@@ -1,21 +1,30 @@
 import os
 import numpy as np
-from skimage.io import imread, imshow, imsave
-import matplotlib.pyplot as plt
+from skimage.io import imread, imsave
 from skimage.color import rgb2gray
 import helper
+from PIL import Image
+import matplotlib.pyplot as plt
 
 def process(directoryName):
+    output_dir = os.path.join(os.getcwd(), "output")
+    os.makedirs(output_dir, exist_ok=True)
+
     for filename in os.listdir(directoryName):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            filenamestr = filename.split('.')[0]
-            extension  = filename.split('.')[1]
-            img = imread(os.path.join(directoryName, filename))
+        if filename.lower().endswith(('.jpg', '.png')):
+            file_path = os.path.join(directoryName, filename)
+            img = imread(file_path)
             img_new = rgb2gray(img)
-            fig = plt.figure() 
-            ax1 = fig.add_subplot(221)
-            imshow(img_new)
-            plt.title('Grayscale Format') 
-            imsave(os.getcwd() + "/output/" + filenamestr +"-" + helper.generateRandomName() + "_modified.jpeg", (img_new*255).astype(np.uint8))
+            
+            # Save grayscale image using Pillow for memory efficiency
+            grayscale_img = Image.fromarray((img_new * 255).astype(np.uint8))
+            
+            # Generate a random file name for the modified image
+            random_name = helper.generateRandomName()
+            output_file_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}-{random_name}_modified.jpeg")
+            grayscale_img.save(output_file_path)
+
+            # Optionally close the figure if you were using matplotlib
+            plt.close('all')
         else:
             continue
--- index.py
+++ index.py
@@ -1,20 +1,20 @@
 import bottle
-from bottle import route, run, Response, template
-import json
+from bottle import route, run, template
+from concurrent.futures import ThreadPoolExecutor
 import image
 
 def call_service():
     directoryName = 'photos'
-    image.process(directoryName)
+    # Using a thread pool to process images concurrently
+    with ThreadPoolExecutor() as executor:
+        executor.submit(image.process, directoryName)
 
 @route('/')
 def index():
     """Home page"""
     title = "Image Processor App"
     call_service()
-    return template('index.tpl',data="Request completed!", title=title)
+    return template('index.tpl', data="Request completed!", title=title)
 
 if __name__ == '__main__':
-	run(host='0.0.0.0', port=8000, debug=False, reloader=True)
-	
-serverApp = bottle.default_app()
+    run(host='0.0.0.0', port=8000, debug=False, reloader=True)
